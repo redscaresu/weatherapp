954c5eca86979c7011cd2a4993dbc588c69807bc
redscaresu <redscaresu@users.noreply.github.com>
2021-08-01 16:56:45 +0100

    got the weather out now out of the struct, I need to extract the weather now

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,18 +9,68 @@ import (
 	"os"
 )
 
-func weather(token string) {
+type Weather struct {
+	Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"main"`
+		Description string `json:"description"`
+		Icon        string `json:"icon"`
+	} `json:"weather"`
+	Base string `json:"base"`
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		TempMin   float64 `json:"temp_min"`
+		TempMax   float64 `json:"temp_max"`
+		Pressure  int     `json:"pressure"`
+		Humidity  int     `json:"humidity"`
+	} `json:"main"`
+	Visibility int `json:"visibility"`
+	Wind       struct {
+		Speed float64 `json:"speed"`
+		Deg   int     `json:"deg"`
+	} `json:"wind"`
+	Clouds struct {
+		All int `json:"all"`
+	} `json:"clouds"`
+	Dt  int `json:"dt"`
+	Sys struct {
+		Type    int    `json:"type"`
+		ID      int    `json:"id"`
+		Country string `json:"country"`
+		Sunrise int    `json:"sunrise"`
+		Sunset  int    `json:"sunset"`
+	} `json:"sys"`
+	Timezone int    `json:"timezone"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Cod      int    `json:"cod"`
+}
+
+func GetWeather(token string) {
+
+	var w Weather
+
 	//call open weather map and get something
 	resp, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=london&appid=%s", token))
 	if err != nil {
 		log.Fatal(err)
 	}
 	read_all, err := ioutil.ReadAll(resp.Body)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", read_all)
+
+	err = json.Unmarshal(read_all, &w)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(w.Weather)
 }
 
 func main() {
@@ -30,5 +81,5 @@ func main() {
 		os.Exit(2)
 	}
 
-	weather(token)
+	GetWeather(token)
 }
