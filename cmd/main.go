5459d05cbcd7becc44a3dff086e95bfa5a99733d
redscaresu <redscaresu@users.noreply.github.com>
2021-08-01 16:09:18 +0100

    create a weather function outside of main

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,7 @@ import (
 	"os"
 )
 
-func main() {
-
-	token := os.Getenv("WEATHERAPP_TOKEN")
-	if len(token) == 0 {
-		fmt.Printf("please set a weatherapp token\n")
-		os.Exit(2)
-	}
-
+func weather(token string) {
 	//call open weather map and get something
 	resp, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=london&appid=%s", token))
 	if err != nil {
@@ -27,4 +20,15 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s", read_all)
+}
+
+func main() {
+
+	token := os.Getenv("WEATHERAPP_TOKEN")
+	if len(token) == 0 {
+		fmt.Printf("please set a weatherapp token\n")
+		os.Exit(2)
+	}
+
+	weather(token)
 }
