f715a79c023b2d2256e64048bc069c6caa230a08
redscaresu <redscaresu@users.noreply.github.com>
2021-08-10 11:17:56 +0100

    remove most of the struct, we dont need it

--- weather.go
+++ weather.go
@@ -12,17 +12,12 @@ import (
 )
 
 type Weather struct {
-	Coord struct {
-		Lon float64 `json:"lon"`
-		Lat float64 `json:"lat"`
-	} `json:"coord"`
 	Weather []struct {
 		ID          int    `json:"id"`
 		Main        string `json:"main"`
 		Description string `json:"description"`
 		Icon        string `json:"icon"`
 	} `json:"weather"`
-	Base string `json:"base"`
 	Main struct {
 		Temp      float64 `json:"temp"`
 		FeelsLike float64 `json:"feels_like"`
@@ -31,26 +26,6 @@ type Weather struct {
 		Pressure  int     `json:"pressure"`
 		Humidity  int     `json:"humidity"`
 	} `json:"main"`
-	Visibility int `json:"visibility"`
-	Wind       struct {
-		Speed float64 `json:"speed"`
-		Deg   int     `json:"deg"`
-	} `json:"wind"`
-	Clouds struct {
-		All int `json:"all"`
-	} `json:"clouds"`
-	Dt  int `json:"dt"`
-	Sys struct {
-		Type    int    `json:"type"`
-		ID      int    `json:"id"`
-		Country string `json:"country"`
-		Sunrise int    `json:"sunrise"`
-		Sunset  int    `json:"sunset"`
-	} `json:"sys"`
-	Timezone int    `json:"timezone"`
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Cod      int    `json:"cod"`
 }
 
 type CityUnknown struct {
