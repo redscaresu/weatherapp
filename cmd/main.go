eae50b989bff2f04bce139868e1c07d8a45afaf5
redscaresu <redscaresu@users.noreply.github.com>
2021-08-27 11:43:17 +0100

    handle no location arg

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 
-	location := os.Args[1]
-
-	if len(os.Args) == 0 {
+	if len(os.Args) == 1 {
 		fmt.Fprintf(os.Stderr, "please set a location e.g. london\n")
 		os.Exit(2)
 	}
+
+	location := os.Args[1]
 
 	token := os.Getenv("WEATHERAPP_TOKEN")
 	if len(token) == 0 {
