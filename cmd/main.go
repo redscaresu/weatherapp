aff268a37d108931f8cdd97f8f37dbfdc6340d92
redscaresu <redscaresu@users.noreply.github.com>
2021-10-08 09:15:42 +0100

    move token env variables into the package

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,6 @@ import (
 
 func main() {
 
-	token := os.Getenv("WEATHERAPP_TOKEN")
-	if len(token) == 0 {
-		fmt.Fprintf(os.Stderr, "please set env variable, WEATHERAPP_TOKEN \n")
-		os.Exit(2)
-	}
-
-	resp := weather.CliOutput(token, os.Args)
+	resp := weather.CliOutput(os.Args)
 	fmt.Print(resp)
 }
--- weather.go
+++ weather.go
@@ -40,7 +40,13 @@ type Conditions struct {
 	Celsius float64
 }
 
-func CliOutput(token string, args []string) (output string) {
+func CliOutput(args []string) (output string) {
+
+	token := os.Getenv("WEATHERAPP_TOKEN")
+	if len(token) == 0 {
+		fmt.Fprintf(os.Stderr, "please set env variable, WEATHERAPP_TOKEN \n")
+		os.Exit(2)
+	}
 
 	location := LocationFromArgs(args)
 	url := BuildURL(token, location)
